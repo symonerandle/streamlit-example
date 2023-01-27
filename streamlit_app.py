17536ce9e6fb9fe596f359a42b30364acd0b97b3
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-27 17:10:14 -0600

    adding designer for data loading function

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,10 @@ import pandas as pd
 """
 This is my streamlit app.
 """
-df = pd.read_csv("data/reddit_comments.csv")
+
+@st.experimental_memo
+def load_data():
+    return pd.read_csv("data/reddit_comments.csv")
+    
+df = load_data()
 st.dataframe(df)
