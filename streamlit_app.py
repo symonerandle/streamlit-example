e5ae29ed9b49f04b2f35f2627c59e0699b2f283f
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-28 08:29:13 -0600

    Updated experimental_memo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,10 @@ import pandas as pd
 This is my streamlit app.
 """
 
-@st.experimental_memo
+@st.experimental_memo(show_spinner=True)
 def load_data():
-    tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-    with tab1:
-        pd.read_csv("data/reddit_comments.csv")
-    return
+    return pd.read_csv("data/reddit_comments.csv")
+
 df = load_data()
 st.dataframe(df)
-
 st.button("close grid")
