f933332d86dc7aa5068e1f8284de112a0d54b353
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-28 11:26:46 -0600

    added comments to code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,23 @@
 import streamlit as st
 import pandas as pd
 
-"""
-This is my streamlit app.
-"""
-
+#Container located at top of UI.
 con = st.container()
 con.write("Inside container")
 
+#Designer function to memorize dataframe display.
 @st.experimental_memo #(show_spinner=True)
 def load_data():
-
     return pd.read_csv("data/reddit_comments.csv")
 
+#Save load_data function into df var. access df in streamlit.
 df = load_data()
 st.dataframe(df)
 
+#Side bar area.
 st.sidebar.button("Enter")
+
+
 #def hide_data():
     #return st.button("Show grid", on_click=load_data(df))
 
