996de1426ff5c1f4ea4ac881734d0934ef475772
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-27 20:16:37 -0600

    added 2 tabs

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import pandas as pd
 This is my streamlit app.
 """
 
+tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+
 @st.experimental_memo
 def load_data():
     return pd.read_csv("data/reddit_comments.csv")
