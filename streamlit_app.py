bd2cf8271ac791137e5fb5dbcb26d4344b9d8b0a
Symone Randle <81542002+symonerandle@users.noreply.github.com>
2023-01-27 18:22:17 -0600

    add close grid button

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,8 @@ This is my streamlit app.
 @st.experimental_memo
 def load_data():
     return pd.read_csv("data/reddit_comments.csv")
-    
+
 df = load_data()
 st.dataframe(df)
+
+st.button("close grid")
